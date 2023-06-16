32be06640298968ec38ac71becfdde4041d10f59
Ofuani-Joseph <123358386+Ofuani-Joseph@users.noreply.github.com>
2023-06-17 00:20:41 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
-# Adeyemi Damilare <adeyemidami19@gmail.com>
-
 
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
