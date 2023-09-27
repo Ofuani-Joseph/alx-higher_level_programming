30a7d5c59e7d14ffb50c07f3683102a510b81844
Ofuani-Joseph <josephofuani99@gmail.com>
2023-09-27 15:59:01 -0700

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const lang of languages) console.log(lang);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
